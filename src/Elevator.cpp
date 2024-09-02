007961fc7bc605c4a79b75247236787f3a7a72b8
el-crag <azertyggmf@outlook.com>
2024-09-01 21:45:21 -0500

    Capture first char to go to floor

--- src/Elevator.cpp
+++ src/Elevator.cpp
@@ -1,13 +1,13 @@
 #include "Elevator.hpp"
 
 void Elevator::processMessage(unsigned char * message, size_t length, bool printable) {
-	if (printable) {
-		for (int i = 0; i < length; ++i) {
-			Serial.print((char) message[i]);
-		}
+	unsigned int floor = (unsigned int) message[0] - '0';
+	Serial.println(floor);
+	if (floor > 0 && floor < 4) {
+		CAST_MODULE_POINTER(Machinist, INDEX_MODULE_MACHINIST)->handleTargetFloor(floor);
 	}
+
 	delete[] message;
-	// Execute all instructions here
 }
 
 void Elevator::initializeModulesPointerArray(unsigned int quantity) {

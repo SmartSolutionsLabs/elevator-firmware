37b550f8d281ce5aa1d94183cee0aad0edb99335
SmartSolutionsLabs <163151140+SmartSolutionsLabs@users.noreply.github.com>
2024-09-03 09:29:14 -0500

    getting the correct value of the sensor

--- src/Sensor.cpp
+++ src/Sensor.cpp
@@ -6,34 +6,51 @@ Sensor::Sensor(const char * name, int taskCore) : Module(name, taskCore) {
 void Sensor::connect(void * data) {
 	this->pin = * (uint8_t *) data;
 	pinMode(this->pin , INPUT);
+	this->lastReading = digitalRead(this->pin);
+	this->lastDebounceTime = millis();
+	this->debugTimer = 1000;
 }
 
 void Sensor::run(void* data) {
-	this->iterationDelay = 10 / portTICK_PERIOD_MS;
-
+	this->iterationDelay = 1000 / portTICK_PERIOD_MS;
+	vTaskDelay(this->iterationDelay);
 	while (1) {
 		this->reading = digitalRead(this->pin); // LOW is pressed
-
-		if (this->reading != this->lastButtonState) {
-			this->lastDebounceTime = millis();  // Reset debouncer
-		}
+		/*if (millis() - this->debugTimer > 1000 ){
+			this->debugTimer = millis();
+			Serial.print(this->name);
+			Serial.print("\t value: ");
+			Serial.println(this->buttonState);
+		}*/
 		if ((millis() - this->lastDebounceTime) > this->debounceDelay) {
-			if (this->reading != this->buttonState) {
-				this->buttonState = this->reading;
-				if (this->buttonState == LOW) {  // Button pressed
-					this->machinist->handleArrivedFloor(this->floorIndex, this->buttonState);
-					Serial.printf("Floor %d detected\n", this->floorIndex);
-				}
+			this->lastDebounceTime = millis();
+
+			if (this->reading == false && this->lastReading == true){
+				this->lastButtonState = this->buttonState;
+				this->buttonState = true;
+				this->lastReading = this->reading;
+				this->machinist->handleArrivedFloor(this->floorIndex, this->buttonState);
+				Serial.printf("Floor %d arrived\n", this->floorIndex);
+			}
+			else if (this->reading == true && this->lastReading == false){
+				this->lastButtonState = this->buttonState;
+				this->buttonState = false;
+				this->lastReading = this->reading;
+				this->machinist->handleArrivedFloor(this->floorIndex, this->buttonState);
+				Serial.printf("Floor %d leaved\n", this->floorIndex);
 			}
 		}
 		this->lastButtonState = this->reading;
 
 		// A little delay to avoid many reads
-		vTaskDelay(this->iterationDelay);
+		//
 	}
 }
 
 bool Sensor::getValue(){
-	bool sensorState = !(digitalRead(this->pin));
-	return sensorState;
+	return this->buttonState;
+}
+
+bool Sensor::getStartValue(){
+	return !digitalRead(this->pin);
 }
